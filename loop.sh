decbe5b13e0c1b07652036d0289e72f3c0594d3c
Jake Howard <git@theorangeone.net>
2020-04-25 14:33:13 +0100

    Time the report generation

--- loop.sh
+++ loop.sh
@@ -6,7 +6,7 @@ set -e
 while true;
 do
     echo "Generating report..."
-    goaccess /goaccess/access.log -o /var/www/goaccess/report.html --log-format=COMBINED --no-progress --no-parsing-spinner
+    time goaccess /goaccess/access.log -o /var/www/goaccess/report.html --log-format=COMBINED --no-progress --no-parsing-spinner
     echo "Finished, waiting 60 seconds"
     sleep 60
 done
