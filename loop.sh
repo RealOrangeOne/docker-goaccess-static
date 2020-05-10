13f4b1a37ebc7fb279ab3f2809a0751665383946
Jake Howard <git@theorangeone.net>
2020-05-10 12:24:53 +0100

    Stop the proess faster rather than waiting for `SIGKILL`
    `wait` responds nicely to `SIGTERM`, `sleep` doesn't.

--- loop.sh
+++ loop.sh
@@ -8,5 +8,6 @@ do
     echo "Generating report..."
     time goaccess /goaccess/access.log -o /var/www/goaccess/report.html --log-format=COMBINED --no-progress --no-parsing-spinner
     echo "Finished, waiting 60 seconds"
-    sleep 60
+    sleep 60 &
+    wait $!
 done
